bc8c6ffe3bb6a9a2175ac17403c868739cb429bc
nath@home <nathdwek@ulb.ac.be>
2016-05-25 12:29:06 +0200

    new test

--- test.m
+++ test.m
@@ -2,8 +2,12 @@ clear all; close all;
 
 c = double(imread('castle.png'));
 yuv = frct(c);
-w = wletdec(yuv,3,'bior4.4');
-t = wletcells2array(w);
+w = wletdec(yuv, 4, 'bior4.4');
+iyuv = wletrec(w, 'bior4.4');
+ic = irct(iyuv);
 figure;
-imshow(uint8(t(:,:,1)));
-
+subplot(121)
+imshow(uint8(c));
+subplot(122);
+imshow(uint8(ic));
+title(sprintf('PSNR = %g', psnr(round(c),round(ic))))
